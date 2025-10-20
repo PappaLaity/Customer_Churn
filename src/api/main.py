c80b2faeed43c9fbe85e91450ed88119d143e3d3
Pappalaity <pappalaity@gmail.com>
2025-10-20 21:53:54 +0000

    Init Project

---
+++ src/api/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy import create_engine, select
+from sqlmodel import SQLModel, Session
+
+from src.api.utils.enum.UserRole import UserRole
+from src.api.entities.users import User
+
+
+app = FastAPI(title="Customer Churn Prediction")
+
+from pydantic import BaseModel
+
+class UserRead(BaseModel):
+    id: int
+    username: str
+    email: str
+    phone: str
+    role: str
+
+    class Config:
+        orm_mode = True
+
+# engine = create_engine("sqlite:///database.db")
+DATABASE_URL = "mysql+pymysql://root@localhost/churn_test"
+engine = create_engine(DATABASE_URL, echo=True)
+
+SQLModel.metadata.create_all(engine)
+
+
+@app.get("/",response_model=UserRead)
+async def home():
+
+    with Session(engine) as session:
+        statement = select(User).where(User.username == "Username")
+        user = session.exec(statement).first()
+        print(type(user))
+        # user = user.model_dump()
+        # print(user)
+        # user = User(
+        #     username="Username",
+        #     email="username1@example.com",
+        #     phone="+221772343321",
+        #     password="secret",
+        #     role=UserRole.ADMIN
+        # )
+        # session.add(user)
+        # session.commit()
+    # return user.model_dump()
+    return {"results": user}
+
+
+@app.get("/health")
+async def check_healh():
+    return {"check": "I'm ok! No worry"}
